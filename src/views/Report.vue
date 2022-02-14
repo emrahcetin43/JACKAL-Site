e91a7ba665f66547d8a00643fdc8a9c3d38a6a8f
Erin Rivas <erin@atomicbear.com>
2022-02-14 10:40:57 -0600

    added form instructions

--- src/views/Report.vue
+++ src/views/Report.vue
@@ -1,5 +1,13 @@
 <template>
   <div class="report">
+    <div class="directions">
+      <p>
+        Thank you for participating in our open Alpha! Please use the below form to submit bug reports with as much detail as you can provide.
+      </p>
+      <p>
+        To reduce the volume of tickets, please do not submit a ticket if everything is working, and instead share your feedback and experience with us via <a href="https://twitter.com/JACKAL_DAO">Twitter</a> or <a href="https://discord.gg/5GKym3p6rj">Discord</a>!
+      </p>
+    </div>
     <form name="bug_report" method="POST" data-netlify="true" data-netlify-honeypot="bot-field">
       <input type="hidden" name="form-name" value="bug_report" />
       <p>
@@ -46,9 +54,13 @@ export default {
 </script>
 
 <style scoped>
+.directions {
+  font-size: 20px;
+}
 .report {
   width: 100%;
   display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
 }
